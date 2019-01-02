14b4273f4b053ed3822512a4b511a62995334291
Danilo Novakovic <dakenzi97@gmail.com>
2019-01-02 14:36:58 +0100

    Made DataGrid bindable

--- PZ3-NetworkService/ViewModel/NetworkDataViewModel.cs
+++ PZ3-NetworkService/ViewModel/NetworkDataViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace PZ3_NetworkService.ViewModel
 {
     public class NetworkDataViewModel : BindableBase
     {
+        public static BindingList<Model.ReactorModel> ReactorList { get; private set; } = new BindingList<Model.ReactorModel>();
+        static NetworkDataViewModel()
+        {
+            RefreshList();
+        }
+        private static void RefreshList()
+        {
+            ReactorList.Clear();
+            foreach (Model.ReactorModel reactor in Database.Reactors.Values)
+            {
+                ReactorList.Add(reactor);
+            }
+        }
+
     }
 }
