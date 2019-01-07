1885ab21bb8472edf6ccf3fcadf063e265e4aa3a
Danilo Novakovic <dakenzi97@gmail.com>
2019-01-07 13:35:59 +0100

    Created Bindable Rich Text box and binded it to Flow Doc inside my View Model. Works as intended

--- PZ3-NetworkService/MainWindowViewModel.cs
+++ PZ3-NetworkService/MainWindowViewModel.cs
@@ -20,7 +20,7 @@ namespace PZ3_NetworkService
         public MainWindowViewModel()
         {
             this.NavCommand = new MyICommand<string>(this.OnNav);
-            this.CurrentViewModel = this.dataChartViewModel;
+            this.CurrentViewModel = this.reportViewModel;
         }
 
         public BindableBase CurrentViewModel
