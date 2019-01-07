aa11dca4c573687f030b36c8f6c1ef0acd06ac26
Danilo Novakovic <dakenzi97@gmail.com>
2019-01-07 11:51:06 +0100

    Changed Database to use SortedDictionaries instead of Dictionary for faster queries (and sorted ids)

--- PZ3-NetworkService/Database.cs
+++ PZ3-NetworkService/Database.cs
@@ -9,15 +9,15 @@ namespace PZ3_NetworkService
     public static class Database
     {
         private static DataIO serializer = new DataIO();
-        public static Dictionary<int, Model.ReactorModel> Reactors { get; private set; }
+        public static SortedDictionary<int, Model.ReactorModel> Reactors { get; private set; }
         public static List<int> ReactorIds { get; private set; }
-        public static Dictionary<string, Model.ReactorTypeModel> ReactorTypes { get; private set; }
+        public static SortedDictionary<string, Model.ReactorTypeModel> ReactorTypes { get; private set; }
 
         static Database()
         {
-            Database.Reactors = Database.Load() ?? new Dictionary<int, Model.ReactorModel>();
+            Database.Reactors = Database.Load() ?? new SortedDictionary<int, Model.ReactorModel>();
             Database.ReactorIds = Database.Reactors.Keys.ToList();
-            Database.ReactorTypes = new Dictionary<string, Model.ReactorTypeModel>()
+            Database.ReactorTypes = new SortedDictionary<string, Model.ReactorTypeModel>()
             {
                 { "thermal", new Model.ReactorTypeModel("thermal", "/Images/thermal_power_plant.jpg")},
                 { "fusion", new Model.ReactorTypeModel("fusion","/Images/fusion_power_plant.jpg") }
@@ -76,12 +76,12 @@ namespace PZ3_NetworkService
         /// <summary>
         /// Attempts to DeSerialize database from fileName. 
         /// </summary>
-        public static Dictionary<int, Model.ReactorModel> Load(string fileName = "database.xml")
+        public static SortedDictionary<int, Model.ReactorModel> Load(string fileName = "database.xml")
         {
             try
             {
                 List<Model.ReactorModel> reactors = serializer.DeSerializeObject<List<Model.ReactorModel>>(fileName);
-                var retVal = new Dictionary<int, Model.ReactorModel>();
+                var retVal = new SortedDictionary<int, Model.ReactorModel>();
                 foreach(var reactor in reactors)
                 {
                     retVal[reactor.Id] = reactor;
