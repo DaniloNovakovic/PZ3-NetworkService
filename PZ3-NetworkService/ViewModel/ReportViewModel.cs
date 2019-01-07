cca4b9940b8f60d44d635da514a63d7bfbd86a02
Danilo Novakovic <dakenzi97@gmail.com>
2019-01-07 15:18:22 +0100

    Revert "Revert "Created Bindable Rich Text box and binded it to Flow Doc inside my View Model. Works as intended""
    This reverts commit 893be460466c6c5140819e75d2f1bbcd16e52860.

--- PZ3-NetworkService/ViewModel/ReportViewModel.cs
+++ PZ3-NetworkService/ViewModel/ReportViewModel.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace PZ3_NetworkService.ViewModel
 {
@@ -51,9 +54,20 @@ namespace PZ3_NetworkService.ViewModel
                 }
             }
         }
+        private FlowDocument flowDoc = new FlowDocument();
+        public FlowDocument FlowDoc
+        {
+            get => this.flowDoc;
+            set
+            {
+                this.flowDoc = value;
+                this.OnPropertyChanged("FlowDoc");
+            }
+        }
+
         public ReportViewModel()
         {
-            this.startDate = DateTime.ParseExact("01/01/2019", "dd/MM/yyyy", CultureInfo.InvariantCulture) ;
+            this.startDate = DateTime.ParseExact("01/01/2019", "dd/MM/yyyy", CultureInfo.InvariantCulture);
             this.endDate = DateTime.Today;
             this.ShowCommand = new MyICommand(this.OnShow);
         }
@@ -66,7 +80,53 @@ namespace PZ3_NetworkService.ViewModel
                 DateTime date = DateTime.ParseExact(sdate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
                 return DateTime.Compare(date, this.startDate) >= 0 && DateTime.Compare(date, this.endDate) <= 0;
             });
-            this.TextReport = this.ConvertLogDictToStr(logDict);
+            //this.TextReport = this.ConvertLogDictToStr(logDict);
+            this.FlowDoc = this.ConvertLogToFlowDocument(logDict);
+        }
+        private FlowDocument ConvertLogToFlowDocument(Dictionary<int, List<string>> logDict)
+        {
+            FlowDocument doc = new FlowDocument();
+            Paragraph paragraph = new Paragraph(new Run("REPORT:"))
+            {
+                Foreground = new SolidColorBrush(Colors.Purple),
+                FontWeight = FontWeights.Bold,
+                FontSize = 27
+            };
+            doc.Blocks.Add(paragraph);
+
+            List<int> ids = logDict.Keys.Where(x => Database.Reactors.ContainsKey(x)).ToList();
+            ids.Sort();
+            foreach (int id in ids)
+            {
+                paragraph = new Paragraph(new Run($"- {Database.Reactors[id].Name}, ID: {id}"))
+                {
+                    Foreground = new SolidColorBrush(Colors.Purple),
+                    FontWeight = FontWeights.Bold,
+                    FontSize = 17
+                };
+                doc.Blocks.Add(paragraph);
+                foreach (string el in logDict[id])
+                {
+                    Color col = Colors.Green;
+                    var match = Regex.Match(el, @".*?([0-9.]+)$");
+                    if(double.TryParse(match.Groups[1].Value, out double val))
+                    {
+                        if(val < Model.ReactorModel.MIN_SAFE_TEMP_CELS || val > Model.ReactorModel.MAX_SAFE_TEMP_CELS)
+                        {
+                            col = Colors.Red;
+                        }
+                    }
+                    paragraph = new Paragraph(new Run($"\t{el}"))
+                    {
+                        Foreground = new SolidColorBrush(col),
+                        FontWeight = FontWeights.Bold,
+                        FontSize = 13
+                    };
+                    doc.Blocks.Add(paragraph);
+                }
+                doc.Blocks.Add(new Paragraph(new Run(Environment.NewLine)));
+            }
+            return doc;
         }
         private string ConvertLogDictToStr(Dictionary<int, List<string>> logDict)
         {
